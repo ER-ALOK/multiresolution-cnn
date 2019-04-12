c3d25e9122b6c543fcd0ca3d2589a731d0b5c49a
ALOK TIWARI <34236103+ER-ALOK@users.noreply.github.com>
2019-04-12 17:18:35 +0530

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -1,5 +1,5 @@
 import os
-import Keras
+import keras
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
 from keras.preprocessing.image import ImageDataGenerator
